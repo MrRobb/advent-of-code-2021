abaf11be71c50dbf8ef6ea603b7a9400c448fb26
Mr.Robb <rob.ariosa@hotmail.com>
2021-12-04 15:36:23 +0000

    Day 1 - Part 1: Added

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,17 @@
+
+use std::fs::read_to_string;
+use itertools::Itertools;
+
+fn calculate_increases(input: &str) -> usize {
+    input.lines()
+        .flat_map(str::parse::<u64>)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count()
+}
+
 pub fn main() {
-    println!("Not implemented yet");
+    let input = read_to_string("input/day1/input1.txt")
+        .expect("Input file not found");
+    println!("RESULT = {}", calculate_increases(&input));
 }
