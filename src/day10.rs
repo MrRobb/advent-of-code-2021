4ce3f5629cfb09af89d3fb73fae8c2efafa0567d
Roberto <rob.ariosa@hotmail.com>
2021-12-11 21:40:59 +0100

    Formatting

--- src/day10.rs
+++ src/day10.rs
@@ -9,121 +9,114 @@ fn median(numbers: &mut [usize]) -> usize {
 }
 
 fn match_brackets(c: char) -> char {
-    match c {
-        '(' => ')',
-        '[' => ']',
-        '{' => '}',
-        '<' => '>',
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-        '>' => '<',
-        _ => unreachable!()
-    }
+	match c {
+		'(' => ')',
+		'[' => ']',
+		'{' => '}',
+		'<' => '>',
+		')' => '(',
+		']' => '[',
+		'}' => '{',
+		'>' => '<',
+		_ => unreachable!(),
+	}
 }
 
 pub fn calculate_incorrect(input: &str) -> usize {
-	input.lines()
-        .map(|line| {
-            let mut stack = Vec::new();
-            let mut incorrect = None;
-            line.chars()
-                .try_fold(&mut stack, |stack, c| {
-                    match c {
-                        '(' | '[' | '{' | '<' => {
-                            stack.push(c);
-                            Some(stack)
-                        },
-                        ')' | ']' | '}' | '>' => {
-                            if *stack.last().unwrap() == match_brackets(c) {
-                                stack.pop();
-                                Some(stack)
-                            }
-                            else {
-                                incorrect = Some(c);
-                                None
-                            }
-                        } 
-                        _ => unreachable!()
-                    }
-                });
+	input
+		.lines()
+		.map(|line| {
+			let mut stack = Vec::new();
+			let mut incorrect = None;
+			line.chars().try_fold(&mut stack, |stack, c| match c {
+				'(' | '[' | '{' | '<' => {
+					stack.push(c);
+					Some(stack)
+				},
+				')' | ']' | '}' | '>' => {
+					if *stack.last().unwrap() == match_brackets(c) {
+						stack.pop();
+						Some(stack)
+					}
+					else {
+						incorrect = Some(c);
+						None
+					}
+				},
+				_ => unreachable!(),
+			});
 
-            // Correct and complete
-            if stack.is_empty() {
-                0
-            }
-            else {
-                match incorrect {
-                    // Incorrect
-                    Some(')') => 3,
-                    Some(']') => 57,
-                    Some('}') => 1197,
-                    Some('>') => 25137,
-                    Some(_) => unreachable!(),
+			// Correct and complete
+			if stack.is_empty() {
+				0
+			}
+			else {
+				match incorrect {
+					// Incorrect
+					Some(')') => 3,
+					Some(']') => 57,
+					Some('}') => 1197,
+					Some('>') => 25137,
+					Some(_) => unreachable!(),
 
-                    // Correct and incomplete
-                    None => 0,
-                }
-            }
-        })
-        .sum()
+					// Correct and incomplete
+					None => 0,
+				}
+			}
+		})
+		.sum()
 }
 
 pub fn calculate_incomplete(input: &str) -> usize {
-	let mut scores = input.lines()
-        .map(|line| {
-            let mut stack = Vec::new();
-            let mut incorrect = None;
-            line.chars()
-                .try_fold(&mut stack, |stack, c| {
-                    match c {
-                        '(' | '[' | '{' | '<' => {
-                            stack.push(c);
-                            Some(stack)
-                        },
-                        ')' | ']' | '}' | '>' => {
-                            if *stack.last().unwrap() == match_brackets(c) {
-                                stack.pop();
-                                Some(stack)
-                            }
-                            else {
-                                incorrect = Some(c);
-                                None
-                            }
-                        } 
-                        _ => unreachable!()
-                    }
-                });
+	let mut scores = input
+		.lines()
+		.map(|line| {
+			let mut stack = Vec::new();
+			let mut incorrect = None;
+			line.chars().try_fold(&mut stack, |stack, c| match c {
+				'(' | '[' | '{' | '<' => {
+					stack.push(c);
+					Some(stack)
+				},
+				')' | ']' | '}' | '>' => {
+					if *stack.last().unwrap() == match_brackets(c) {
+						stack.pop();
+						Some(stack)
+					}
+					else {
+						incorrect = Some(c);
+						None
+					}
+				},
+				_ => unreachable!(),
+			});
 
-            // Correct and complete
-            if stack.is_empty() {
-                0
-            }
-            else {
-                match incorrect {
-                    // Incorrect
-                    Some(_) => 0,
-                    
-                    // Correct and incomplete
-                    None => {
-                        stack.iter()
-                            .rev()
-                            .fold(0, |score, c| {
-                                5 * score + match c {
-                                    '(' => 1,
-                                    '[' => 2,
-                                    '{' => 3,
-                                    '<' => 4,
-                                    _ => unreachable!()
-                                }
-                            })
-                    },
-                }
-            }
-        })
-        .filter(|score| *score != 0)
-        .collect::<Vec<_>>();
-    median(&mut scores)
+			// Correct and complete
+			if stack.is_empty() {
+				0
+			}
+			else {
+				match incorrect {
+					// Incorrect
+					Some(_) => 0,
+
+					// Correct and incomplete
+					None => stack.iter().rev().fold(0, |score, c| {
+						5 * score
+							+ match c {
+								'(' => 1,
+								'[' => 2,
+								'{' => 3,
+								'<' => 4,
+								_ => unreachable!(),
+							}
+					}),
+				}
+			}
+		})
+		.filter(|score| *score != 0)
+		.collect::<Vec<_>>();
+	median(&mut scores)
 }
 
 pub fn main() {
